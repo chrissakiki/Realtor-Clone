ed71932f599e15e60b3340bf73f722870b438d73
Chris Akiki <akikichriss@gmail.com>
2022-10-09 19:39:03 +0300

    routes added + pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
